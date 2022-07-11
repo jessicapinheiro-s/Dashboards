e1d400e234a68936b5fd2d01bf916350c778a6e4
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-07-11 16:40:25 -0300

    ajustando function se

--- Projeto grafico/japp.js
+++ Projeto grafico/japp.js
@@ -29,47 +29,20 @@ function processamentoDados() {
     let qtdJun = Number(qtdSix.value);
     let qtdJul = Number(qtdSeven.value);
 
-    let colunaJan = document.getElementById('j-bar');
-    let colunaFev = document.getElementById('f-bar');
-    let colunaMar = document.getElementById('m-bar');
-
-    if (qtdJan == 10) {
-        colunaJan.style.height = '40px';
-    } else if (qtdFev == 10) {
-        colunaFev.style.height = '40px';
-    } else if(qtdMar == 10){
-        colunaMar.style.height = '40px';
-    }
-/*
-    if (numeroM == 20) {
-        coluna.style.height = '200px';
-    } else if (numeroH == 20) {
-        colunadois.style.height = '200px';
-    } else if (numThree == 20) {
-        colunatres.style.height = '200px';
-    }
-
-    if (numeroM == 30) {
-        coluna.style.height = '300px';
-    } else if (numeroH == 30) {
-        colunadois.style.height = '300px';
-    } else if (numThree == 30) {
-        colunatres.style.height = '300px';
-    }*/
     se(qtdJan, qtdFev, qtdMar);
 }
 
 function se(y, z, j) {
-    let coluna = document.getElementById('col');
-    let colunadois = document.getElementById('colTwo');
-    let colunatres = document.getElementById('colThree');
+    let colunaJan = document.getElementById('j-bar');
+    let colunaFev = document.getElementById('f-bar');
+    let colunaMar = document.getElementById('m-bar');
 
     if (y == 10) {
-        coluna.style.height = '100px';
+        colunaJan.style.height = '100px';
     } else if (z == 10) {
-        colunadois.style.height = '100px';
+        colunaFev.style.height = '100px';
     } else if (j == 10) {
-        colunatres.style.height = '100px';
+        colunaMar.style.height = '100px';
     }
 
     if (y == 20) {
