d8953f9021e031f387cb3aece0010b1230d48329
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-07-25 17:32:48 -0300

    editando js deixando mais curto

--- Projeto grafico/japp.js
+++ Projeto grafico/japp.js
@@ -11,49 +11,37 @@ btEnviar.addEventListener('click', function () {
 
 
 function coletaDados() {
-    let qtdOne = document.getElementById('qtdOne');
-    let qtdTwo = document.getElementById('qtdOne');
-    let qtdThree = document.getElementById('qtdOne');
-    let qtdFour = document.getElementById('qtdOne');
-    let qtdFive = document.getElementById('qtdOne');
-    let qtdSix = document.getElementById('qtdOne');
-    let qtdSeven = document.getElementById('qtdOne');
-    let qtdEight = document.getElementById('qtdOne');
-    let qtdNine = document.getElementById('qtdOne');
-    let qtdTen = document.getElementById('qtdOne');
-    let qtdEleven = document.getElementById('qtdOne');
-    let qtdTen = document.getElementById('qtdOne');
+    let qtds = document.querySelectorAll('[id *=qtd]');
+    let soma = 0;
 
 
-    let qtdJan = Number(qtdOne.value);
-    let qtdFev = Number(qtdTwo.value);
-    let qtdMar = Number(qtdThree.value);
-    let qtdAbr = Number(qtdFour.value);
-    let qtdMai = Number(qtdFive.value);
-    let qtdJun = Number(qtdSix.value);
-    let qtdJul = Number(qtdSeven.value);
-    let qtdAug = Number(qtdEight.value);
-    let qtdSet = Number(qtdNine.value);
-    let qtdOut = Number(qtdTen.value);
-    let qtdNov = Number(qtdEleven.value);
-    let qtdDez = Number(qtdTen.value);
+    for (let i = 0; i < qtds.length; i++) {
+        soma += Number(qtds[i].value);
+    
+        if(Number(qtds[i].value) <= 50){
+            document.getElementById('j-bar').style.height = '40px';
+            document.getElementById('f-bar').style.height = '40px';
+            document.getElementById('m-bar').style.height = '40px';
+            document.getElementById('a-bar').style.height = '40px';
+            document.getElementById('mai-bar').style.height = '40px';
+        }else if(Number(qtds[i].value) <= 100){
+            document.getElementById('j-bar').style.height = '80px';
+        }
+    }
 
-    processamento(qtdJan, qtdFev, qtdMar);
+
+
 }
 
+
+
+
+    //processamento(qtdJan, qtdFev, qtdMar);
+
+
 function processamento(y, z, j) {
-    let colunaJan = document.getElementById('j-bar');
-    let colunaFev = document.getElementById('f-bar');
-    let colunaMar = document.getElementById('m-bar');
-    let colunaAbr = document.getElementById('j-bar');
-    let colunaMai = document.getElementById('f-bar');
-    let colunaJun = document.getElementById('m-bar');
-    let colunaJul = document.getElementById('j-bar');
-    let colunaAug = document.getElementById('f-bar');
-    let colunaSet = document.getElementById('m-bar');
-    let colunaOut = document.getElementById('j-bar');
-    let colunaNov = document.getElementById('f-bar');
-    let colunaDez = document.getElementById('m-bar');
+    let colunas = document.querySelectorAll('[id *=bar]');
+
 
 
 }
