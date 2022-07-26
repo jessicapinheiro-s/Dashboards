22387b958841e5fde5164e9f528b2b976bba4d6d
Jessica Pinheiro <95878650+jessicapinheiro-s@users.noreply.github.com>
2022-07-25 21:49:31 -0300

    tentando ajustar mudança no tam da col

--- Projeto grafico/japp.js
+++ Projeto grafico/japp.js
@@ -9,34 +9,51 @@ btEnviar.addEventListener('click', function () {
     garf.classList.add('mostrar');
 });
 
-
-function coletaDados() {
+document.getElementById('btEnviar').addEventListener('click', function coletaDados() {
     let qtds = document.querySelectorAll('[id *=qtd]');
     let soma = 0;
 
-
     for (let i = 0; i < qtds.length; i++) {
         soma += Number(qtds[i].value);
-    
-        if(Number(qtds[i].value) <= 50){
+
+        if (qtds[i].value == '') {
+            alert('Os campos vazios valerão 0 ..');
+            return;
+        }
+        let colunas = document.querySelectorAll('[id *=bar]');
+
+        if (Number(qtds[i].value <= 10)) {
+            colunas[i].classList.add("colTamDez");
+
+        } else if (Number(qtds[i].value <= 25)) {
+            document.getElementById('j-bar').style.height = '30px';
+            document.getElementById('f-bar').style.height = '20px';
+            document.getElementById('m-bar').style.height = '20px';
+            document.getElementById('a-bar').style.height = '20px';
+            document.getElementById('mai-bar').style.height = '20px';
+            document.getElementById('jun-bar').style.height = '20px';
+        } else if (Number(qtds[i].value) <= 50) {
             document.getElementById('j-bar').style.height = '40px';
             document.getElementById('f-bar').style.height = '40px';
             document.getElementById('m-bar').style.height = '40px';
             document.getElementById('a-bar').style.height = '40px';
             document.getElementById('mai-bar').style.height = '40px';
-        }else if(Number(qtds[i].value) <= 100){
+            document.getElementById('jun-bar').style.height = '40px';
+        } else if (Number(qtds[i].value) <= 100) {
             document.getElementById('j-bar').style.height = '80px';
+            document.getElementById('f-bar').style.height = '80px';
+            document.getElementById('m-bar').style.height = '80px';
+            document.getElementById('a-bar').style.height = '80px';
+            document.getElementById('mai-bar').style.height = '80px';
+            document.getElementById('jun-bar').style.height = '80px';
         }
+
+
     }
 
+    alert(soma);
+})
 
-
-}
-
-
-
-
-    //processamento(qtdJan, qtdFev, qtdMar);
 
 
 function processamento(y, z, j) {
